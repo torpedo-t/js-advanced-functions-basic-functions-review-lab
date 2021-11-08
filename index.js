0b042505efc8f26babdb24d91f4c36614afe6b4e
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-08 12:24:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function saturdayFun(string="roller-skate") {
+    return `This Saturday, I want to ${string}!`
+}
+
+let mondayWork = function() {
+    return `This Monday, I will ${default}.`
+}
+mondayWork(default="go to the office");
