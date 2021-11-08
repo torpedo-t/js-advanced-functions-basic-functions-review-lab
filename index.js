b3b634cdb1b653fa7fea86226fa3754d2fe101cc
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-08 14:04:46 -0600

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,48 @@ function saturdayFun(string="roller-skate") {
     return `This Saturday, I want to ${string}!`
 }
 
-let mondayWork = function() {
-    return `This Monday, I will ${default}.`
+let mondayWork = function(parameter="go to the office") {
+    return `This Monday, I will ${parameter}.`
 }
-mondayWork(default="go to the office");
+
+function wrapAdjective(symbol="*") {
+    return function(string="special") {
+        return `You are ${symbol}${string}${symbol}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {
+      return a+b;
+    },
+    subtract: function(a, b) {
+      return a-b;
+    },
+    multiply: function(a, b) {
+      return a*b;
+    },
+    divide: function(a, b) {
+      return a/b;
+    }
+  }
+  
+  function actionApplyer(start, fns) {
+    for (let i = 0; i < fns.length; i++) {
+      start = fns[i](start);
+    }
+    return start;
+  }
+
+// implement a function called wrapAdjective
+// it should return a function - this inner function should - 
+// take a single parameter that should default to "special"
+// return a string of the form "You are ..."
+// 
+
+// what is the job of this function? is it supposed to return a string, modify an array?
+
+// each function needs a single, clear purpose...............................................
+
+// what data/info does it need in order to do it's job? what arguments, parameters is necessary?
+
+// what am i expecting as a return value? Should be pretty distinct
